fa444fb77943b9758bb8ae65201561c06d5a619e
Mujahidul hasan <135801085+mujahidulhasan@users.noreply.github.com>
2026-07-22 01:47:25 +0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,13 +7,7 @@ export default function Document() {
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="author" content={siteConfig.seo.author} />
-        <meta property="og:title" content={siteConfig.seo.title} />
-        <meta property="og:description" content={siteConfig.seo.description} />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={siteConfig.seo.title} />
-        <meta name="twitter:description" content={siteConfig.seo.description} />
-        <script src="https://kit.fontawesome.com/66ce5ae449.js" crossOrigin="anonymous" defer></script>
+        <link rel="apple-touch-icon" href="/logo.png" />
       </Head>
       <body>
         <Main />
