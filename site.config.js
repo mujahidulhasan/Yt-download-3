cae1051fb57b5328caec811a78fdf834c04e7843
Mujahidul hasan <135801085+mujahidulhasan@users.noreply.github.com>
2026-07-22 00:31:21 +0600

    Create site.config.js

---
+++ site.config.js
@@ -0,0 +1,21 @@
+const siteConfig = {
+  name: 'StreamVault',
+  shortName: 'SV',
+  description: 'Download videos from multiple platforms in high quality',
+  seo: {
+    title: 'StreamVault - Multi-Platform Video Downloader',
+    description: 'Download videos from YouTube, Facebook, Instagram, TikTok and more. Fast, secure, free.',
+    keywords: 'video downloader, youtube downloader, facebook downloader, instagram downloader',
+    author: 'StreamVault',
+  },
+  accentColor: '#f97316',
+  supportEmail: 'support@streamvault.com',
+  copyright: '2026 StreamVault. All rights reserved.',
+  footerLinks: [
+    { name: 'Privacy Policy', href: '/privacy' },
+    { name: 'Terms of Service', href: '/terms' },
+    { name: 'DMCA', href: '/dmca' },
+  ],
+};
+
+module.exports = siteConfig;
