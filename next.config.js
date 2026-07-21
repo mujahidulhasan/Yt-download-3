8d21b48bd12a8af5f98ef893536fdd686328ce81
Mujahidul hasan <135801085+mujahidulhasan@users.noreply.github.com>
2026-07-22 00:46:00 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,30 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-        ],
-      },
-    ];
   },
 };
 
